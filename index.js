d3844db1cd7592e19a21c9ed4b2071fc6845783f
Shilcof <robert.shilcof@gmail.com>
2021-02-01 15:31:10 +0000

    commit tree a6bcd91b06d2a6810f2d656bd03f24b99b11f472

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+
+function returnFirstTwoDrivers(array) {
+    return [array[0], array[1]];
+}
+
+function returnLastTwoDrivers(array) {
+    return [array[array.length-2], array[array.length-1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {return fare * multiplier};
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, operation) {
+    return operation(arrayOfDrivers);
+}
